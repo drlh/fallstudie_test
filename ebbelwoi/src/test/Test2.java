a9bacfad44fcf340b8a1184dd6a8e28a87047cfe
drlh <leo.hellwich@yahoo.de>
2014-09-15 18:31:25 +0200

    1

--- ebbelwoi/src/test/Test2.java
+++ ebbelwoi/src/test/Test2.java
@@ -1,11 +1,16 @@
 package test;
 
 public class Test2 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		System.out.println("Nik ist doof");
 		System.out.println("Guudee, Yolo");
-		
 		//ihr seid doch alle bleed
 		/* blub di blub. Kommentare so damit wir die Doku automatisch erstellen können :) */
+	
+		while (true) {
+			System.out.println("Wo ist Greg?");
+			
+		}
+	
 	}
 }
