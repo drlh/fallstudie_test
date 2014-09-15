42982da5bde2a0b085b44d0eb3686a46a8cfacd3
Fabian A <s130132@student.dhbw-mannheim.de>
2014-09-15 18:36:42 +0200

    asd

--- ebbelwoi/src/test/Test2.java
+++ ebbelwoi/src/test/Test2.java
@@ -2,7 +2,7 @@ package test;
 
 public class Test2 {
 	public static void main(String[] args) {
-		System.out.println("Nik ist doof");
+		System.out.println("Nik ist da");
 		System.out.println("Guudee, Yolo");
 		
 		//ihr seid doch alle bleed
