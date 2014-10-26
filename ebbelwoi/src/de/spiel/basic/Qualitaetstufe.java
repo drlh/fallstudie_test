00384ff6efe1404d45ed10c7b6937ee6088c0941
Niklas <s131454@student.dhbw-mannheim.de>
2014-10-26 16:36:09 +0100

    Kommentare

--- ebbelwoi/src/de/spiel/basic/Qualitaetstufe.java
+++ ebbelwoi/src/de/spiel/basic/Qualitaetstufe.java
@@ -1,52 +1,64 @@
 package de.spiel.basic;
 
-import java.util.Arrays;
+/**
+ * Klasse für Qualitätsstufen
+ *
+ */
+public class Qualitaetstufe {
+	private int[] qualitaetsStufen = { 0, 1, 2, 3, 4, 5, 6 };
+	private String[] qualitaetStufenBez = { "Verschimmelt", "Schlecht",
+			"Ausreichend", "Mittel", "Gut", "Sehr Gut", "Top" };
+	private int aktuelleStufe;
 
-public class Qualitaetstufe
-{
-    private int[] qualitaetsStufen = { 0, 1, 2, 3, 4, 5, 6 };
-    private String[] qualitaetStufenBez = {"Verschimmelt", "Schlecht","Ausreichend","Mittel","Gut","Sehr Gut","Top"};
-    private int aktuelleStufe;
+	/**
+	 * leerer Konstruktor
+	 */
+	public Qualitaetstufe() {
+		this.aktuelleStufe = 6;
+	}
 
-    public Qualitaetstufe()
-    {
-	this.aktuelleStufe = 6;
-    }
+	/**
+	 * Konstruktor mit Stufe
+	 * 
+	 * @param stufe
+	 */
+	public Qualitaetstufe(int stufe) {
+		if (stufe > 6)
+			aktuelleStufe = 6;
+		if (stufe < 0)
+			aktuelleStufe = 0;
+		else
+			aktuelleStufe = qualitaetsStufen[stufe];
+	}
 
-    public Qualitaetstufe(int stufe)
-    {
-	if (stufe > 6) aktuelleStufe= 6;
-	if (stufe < 0) aktuelleStufe= 0;
-	else aktuelleStufe = qualitaetsStufen[stufe];
-    }
+	// GETTER
+	public int getAktuelleStufe() {
+		return aktuelleStufe;
+	}
 
-    public int getAktuelleStufe()
-    {
-        return aktuelleStufe;
-    }
-    
-    public String getAktuelleBez()
-    {
-        return qualitaetStufenBez[aktuelleStufe];
-    }
+	// GETTER
+	public String getAktuelleBez() {
+		return qualitaetStufenBez[aktuelleStufe];
+	}
 
-    public void qualitaetMindern()
-    {
-        if (aktuelleStufe > 0) this.aktuelleStufe -= 1;
-    }
+	/**
+	 * Mindert Qualitätsstufe
+	 */
+	public void qualitaetMindern() {
+		if (aktuelleStufe > 0)
+			this.aktuelleStufe -= 1;
+	}
 
-    public void setAktuelleStufe(int aktuelleStufe)
-    {
-        if (aktuelleStufe <= 6 && aktuelleStufe >= 0) this.aktuelleStufe = aktuelleStufe;
-    }
+	// SETTER
+	public void setAktuelleStufe(int aktuelleStufe) {
+		if (aktuelleStufe <= 6 && aktuelleStufe >= 0)
+			this.aktuelleStufe = aktuelleStufe;
+	}
 
-    @Override
-    public String toString()
-    {
-	return "Qualitaetstufe [aktuelleStufe= "+aktuelleStufe+", bezeichnung="+getAktuelleBez()+"]";
-    }
-    
-    
-    
-    
+	@Override
+	public String toString() {
+		return "Qualitaetstufe [aktuelleStufe= " + aktuelleStufe
+				+ ", bezeichnung=" + getAktuelleBez() + "]";
+	}
+
 }
--- ebbelwoi/src/de/spiel/basic/Rohstoff.java
+++ ebbelwoi/src/de/spiel/basic/Rohstoff.java
@@ -1,70 +1,88 @@
 package de.spiel.basic;
 
-public class Rohstoff
-{
-    private String name;
-    private double menge;
-    private Qualitaetstufe qualitaet;
+public class Rohstoff {
+	private String name;
+	private double menge;
+	private Qualitaetstufe qualitaet;
 
-    public Rohstoff()
-    {
-	this.name = "Äpfel";
-	this.menge = 0.0;
-	this.qualitaet = new Qualitaetstufe();
-    }
-
-    public Rohstoff(double menge)
-    {
-	this.name = "Äpfel";
-	this.menge = menge;
-	this.qualitaet = new Qualitaetstufe();
-    }
-
-    public Rohstoff(double menge, int qualitaet)
-    {
-	this.name = "Äpfel";
-	this.menge = menge;
-	this.qualitaet = new Qualitaetstufe(qualitaet);
-    }
-
-    public double getMenge()
-    {
-	int qualitaet = this.getQualitaet().getAktuelleStufe();
-	double menge = 0.0;
-	
-	switch (qualitaet) {
-	case 6:	return this.menge;
-	case 5: return (this.menge * 0.9);
-	case 4: return (this.menge * 0.8);
-	case 3: return (this.menge * 0.6);
-	case 2:	return (this.menge * 0.5);
-	case 1:	return (this.menge * 0.3);
-	default: return 0;
+	/**
+	 * leerer Konstruktor
+	 */
+	public Rohstoff() {
+		this.name = "Äpfel";
+		this.menge = 0.0;
+		this.qualitaet = new Qualitaetstufe();
 	}
-	
-    }
 
-    public void setMenge(double menge)
-    {
-	this.menge = menge;
-    }
+	/**
+	 * Konstruktor mit Mengenangabe
+	 * 
+	 * @param menge
+	 */
+	public Rohstoff(double menge) {
+		this.name = "Äpfel";
+		this.menge = menge;
+		this.qualitaet = new Qualitaetstufe();
+	}
 
-    public Qualitaetstufe getQualitaet()
-    {
-	return qualitaet;
-    }
+	/**
+	 * Konstruktor mit Mengen- und Qualitätsangabe
+	 * 
+	 * @param menge
+	 * @param qualitaet
+	 */
+	public Rohstoff(double menge, int qualitaet) {
+		this.name = "Äpfel";
+		this.menge = menge;
+		this.qualitaet = new Qualitaetstufe(qualitaet);
+	}
 
-    public void setQualitaet(int qualitaet)
-    {
-	this.qualitaet.setAktuelleStufe(qualitaet);
-    }
+	/**
+	 * GETTER für Menge abhängig von Qualitätsstufe
+	 * 
+	 * @return
+	 */
+	public double getMenge() {
+		int qualitaet = this.getQualitaet().getAktuelleStufe();
 
-    @Override
-    public String toString()
-    {
-	return "Rohstoff [name=" + name + ", menge=" + menge + ", qualitaet="+ qualitaet + "]";
-    }
-    
-    
+		switch (qualitaet) {
+		case 6:
+			return this.menge;
+		case 5:
+			return (this.menge * 0.9);
+		case 4:
+			return (this.menge * 0.8);
+		case 3:
+			return (this.menge * 0.6);
+		case 2:
+			return (this.menge * 0.5);
+		case 1:
+			return (this.menge * 0.3);
+		default:
+			return 0;
+		}
+
+	}
+
+	// SETTER
+	public void setMenge(double menge) {
+		this.menge = menge;
+	}
+
+	// GETTER
+	public Qualitaetstufe getQualitaet() {
+		return qualitaet;
+	}
+
+	// SETTER
+	public void setQualitaet(int qualitaet) {
+		this.qualitaet.setAktuelleStufe(qualitaet);
+	}
+
+	@Override
+	public String toString() {
+		return "Rohstoff [name=" + name + ", menge=" + menge + ", qualitaet="
+				+ qualitaet + "]";
+	}
 
 }
