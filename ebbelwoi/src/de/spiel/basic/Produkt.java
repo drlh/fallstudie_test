691f194e5a6d505fc725f88a40f5e4a9d32f5d70
Niklas <s131454@student.dhbw-mannheim.de>
2014-10-26 16:30:43 +0100

    .

--- ebbelwoi/src/de/spiel/basic/Produkt.java
+++ ebbelwoi/src/de/spiel/basic/Produkt.java
@@ -1,69 +1,85 @@
 package de.spiel.basic;
 
-public class Produkt
-{
-    private String name;
-    private double menge;
-    private Qualitaetstufe qualitaet;
+public class Produkt {
+	private String name;
+	private double menge;
+	private Qualitaetstufe qualitaet;
 
-    public Produkt()
-    {
-	this.name = "Apfelwein";
-	this.menge = 0.0;
-	this.qualitaet = new Qualitaetstufe();
-    }
-
-    public Produkt(double menge)
-    {
-	this.name = "Apfelwein";
-	this.menge = menge;
-	this.qualitaet = new Qualitaetstufe();
-    }
-
-    public Produkt(double menge, int qualitaet)
-    {
-	this.name = "Apfelwein";
-	this.menge = menge;
-	this.qualitaet = new Qualitaetstufe(qualitaet);
-    }
-
-    public double getMenge()
-    {
-	int qualitaet = this.getQualitaet().getAktuelleStufe();
-	double menge = 0.0;
-	
-	switch (qualitaet) {
-	case 6:	return this.menge;
-	case 5: return (this.menge * 0.9);
-	case 4: return (this.menge * 0.8);
-	case 3: return (this.menge * 0.6);
-	case 2:	return (this.menge * 0.5);
-	case 1:	return (this.menge * 0.3);
-	default: return 0;
+	/**
+	 * leerer Konstruktor
+	 */
+	public Produkt() {
+		this.name = "Apfelwein";
+		this.menge = 0.0;
+		this.qualitaet = new Qualitaetstufe();
 	}
-	
-    }
 
-    public void setMenge(double menge)
-    {
-	this.menge = menge;
-    }
+	/**
+	 * Konstruktor mit Mengenangabe
+	 * @param menge
+	 */
+	public Produkt(double menge) {
+		this.name = "Apfelwein";
+		this.menge = menge;
+		this.qualitaet = new Qualitaetstufe();
+	}
 
-    public Qualitaetstufe getQualitaet()
-    {
-	return qualitaet;
-    }
+	/**
+	 * Konstruktor mit Mengen- und Qualitätsangabe
+	 * @param menge
+	 * @param qualitaet
+	 */
+	public Produkt(double menge, int qualitaet) {
+		this.name = "Apfelwein";
+		this.menge = menge;
+		this.qualitaet = new Qualitaetstufe(qualitaet);
+	}
 
-    public void setQualitaet(int qualitaet)
-    {
-	this.qualitaet.setAktuelleStufe(qualitaet);
-    }
+	/**
+	 * GETTER für Menge abhängig von Qualitätsstufe
+	 * @return
+	 */
+	public double getMenge() {
+		int qualitaet = this.getQualitaet().getAktuelleStufe();
+		
+		switch (qualitaet) {
+		case 6:
+			return this.menge;
+		case 5:
+			return (this.menge * 0.9);
+		case 4:
+			return (this.menge * 0.8);
+		case 3:
+			return (this.menge * 0.6);
+		case 2:
+			return (this.menge * 0.5);
+		case 1:
+			return (this.menge * 0.3);
+		default:
+			return 0;
+		}
 
-    @Override
-    public String toString()
-    {
-	return "Produkt [name=" + name + ", menge=" + menge + ", qualitaet="+ qualitaet + "]";
-    }
-    
-    
+	}
+
+	//SETTER
+	public void setMenge(double menge) {
+		this.menge = menge;
+	}
+
+	// GETTER
+	public Qualitaetstufe getQualitaet() {
+		return qualitaet;
+	}
+
+	// SETTER
+	public void setQualitaet(int qualitaet) {
+		this.qualitaet.setAktuelleStufe(qualitaet);
+	}
+
+	@Override
+	public String toString() {
+		return "Produkt [name=" + name + ", menge=" + menge + ", qualitaet="
+				+ qualitaet + "]";
+	}
+
 }
