6639d1bf73768bbbfef5443b62a799122b2c6c82
drlh <leo.hellwich@yahoo.de>
2014-10-27 03:04:30 +0100

    a<s<as<

--- ebbelwoi/src/de/spiel/GUI/RundenView.java
+++ ebbelwoi/src/de/spiel/GUI/RundenView.java
@@ -927,11 +927,19 @@ public class RundenView extends javax.swing.JFrame implements
 	public void valueChanged(ListSelectionEvent e) {
 		Object src = e.getSource();
 
-		lblVerkaufProdukt.setText(tableVerkaufProd.getModel()
-				.getValueAt(tableVerkaufProd.getSelectedRow()+1, 0).toString());
-
-		lblEinkaufProdukt.setText(tableEinkaufProd.getModel()
-				.getValueAt(tableEinkaufProd.getSelectedRow()+1, 0).toString());
+		if (src.equals(tableVerkaufProd)) {
+			lblVerkaufProdukt.setText(tableVerkaufProd.getModel()
+					.getValueAt(tableVerkaufProd.getSelectedRow(), 0)
+					.toString());
+		}
+		if (src.equals(tableEinkaufProd)) {
+			lblEinkaufProdukt.setText(tableEinkaufProd.getModel()
+					.getValueAt(tableEinkaufProd.getSelectedRow(), 0)
+					.toString());
+			System.out.println(tableEinkaufProd.getModel()
+					.getValueAt(tableEinkaufProd.getSelectedRow(), 0)
+					.toString());
+		}
 
 	}
 
@@ -959,37 +967,7 @@ public class RundenView extends javax.swing.JFrame implements
 	private void loadPlayerData(int player) {
 		loadBilanz(player);
 		loadMitarbeiterReiter(player);
-		loadLager(player);
 		this.player = player;
-	}
-
-	private void loadLager(int player) {
-		DefaultTableModel modFE = (DefaultTableModel) tableLagerFE.getModel();
-		modFE.setRowCount(0);
-		int cFE = Spiel.getSpieler().get(player).getUnternehmen().getLager()
-				.getLagerlisteProdukt().size();
-		for (int i = 0; i < cFE; i++) {
-			modFE.addRow(new Object[] {
-					Spiel.getSpieler().get(player).getUnternehmen().getLager()
-							.getLagerlisteProdukt().get(i).getBezeichnung(),
-					Spiel.getSpieler().get(player).getUnternehmen().getLager()
-							.getLagerlisteProdukt().get(i).getMenge() });
-		}
-		tableLagerFE.setModel(modFE);
-
-		DefaultTableModel modR = (DefaultTableModel) tableLagerRohstoffe
-				.getModel();
-		modR.setRowCount(0);
-		int cR = Spiel.getSpieler().get(player).getUnternehmen().getLager()
-				.getLagerlisteRohstoffe().size();
-		for (int i = 0; i < cR; i++) {
-			modR.addRow(new Object[] {
-					Spiel.getSpieler().get(player).getUnternehmen().getLager()
-							.getLagerlisteRohstoffe().get(i).getBezeichnung(),
-					Spiel.getSpieler().get(player).getUnternehmen().getLager()
-							.getLagerlisteRohstoffe().get(i).getMenge() });
-		}
-		tableLagerRohstoffe.setModel(modR);
 	}
 
 	private void loadBilanz(int player) {
